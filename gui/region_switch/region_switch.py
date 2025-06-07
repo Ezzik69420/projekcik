c5e5a17b2f9112c1be56e63a3e1b5c0788969f59
Ezzik69420 <igorowski6@gmail.com>
2025-06-08 00:16:33 +0200

    Update region_switch.py

--- gui/region_switch/region_switch.py
+++ gui/region_switch/region_switch.py
@@ -1,18 +1,23 @@
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QRadioButton, QLabel, QButtonGroup
+
 class RegionSwitch(QWidget):
     def __init__(self, callback=None):
         super().__init__()
+
         layout = QHBoxLayout()
         layout.addWidget(QLabel("Region:"))
+
         self.group = QButtonGroup(self)
         self.eu_btn = QRadioButton("Europa")
         self.pl_btn = QRadioButton("Polska")
         self.eu_btn.setChecked(True)
         self.group.addButton(self.eu_btn)
         self.group.addButton(self.pl_btn)
+
         if callback:
             self.eu_btn.toggled.connect(lambda: callback("EU"))
             self.pl_btn.toggled.connect(lambda: callback("PL"))
+
         layout.addWidget(self.eu_btn)
         layout.addWidget(self.pl_btn)
         self.setLayout(layout)
