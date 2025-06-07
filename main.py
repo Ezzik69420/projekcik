89612c9dcee6abc4dfc10ca1d6e2c1c99fc2b416
Ezzik69420 <igorowski6@gmail.com>
2025-06-08 00:14:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from PyQt5.QtWidgets import QApplication
-from common.config import Config
+from config.config import Config
 from data.repository import ExcelVehicleDataRepository
 from data.data_service import VehicleDataService
 from export.pdf_exporter import PDFExportStrategy
 from gui.main_window.main_window import MainWindow
 import sys, os
+
 def main():
     os.environ["QTWEBENGINE_DISABLE_SANDBOX"] = "1"
     os.environ["QTWEBENGINE_DISABLE_GPU"] = "1"
     os.environ["QT_QUICK_BACKEND"] = "software"
+
     app = QApplication(sys.argv)
+
     config = Config()
+
     repository = ExcelVehicleDataRepository(config.ev_data_path, config.env_data_path)
     service = VehicleDataService(repository)
     exporter = PDFExportStrategy()
+
     window = MainWindow(service, exporter)
     window.show()
+
     sys.exit(app.exec())
+
 if __name__ == "__main__":
     main()
